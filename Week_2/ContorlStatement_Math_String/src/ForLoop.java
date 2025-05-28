096571d019759fa1e2ddccd81030a503c3fddfe2
kcabhi024 <162951419+kcabhi024@users.noreply.github.com>
2025-05-28 13:31:06 +0545

    week3day1

---
+++ Week_2/ContorlStatement_Math_String/src/ForLoop.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("for loop");
+
+        for(int i=1; i<=100; i += 2){
+            System.out.println(i + " ");
+        }
+
+    }
+}
